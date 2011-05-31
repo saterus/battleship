a0a99085bc29eebba2cb061542a21ce95fa5dd29
Alex Burkhart <saterus@gmail.com>
2011-05-31 13:23:56 -0400

    Implementation for the Firing Controller.

---
+++ src/controllers/FiringControllerImpl.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import models.BattleGrid;
+
+/**
+ * Tracks a target grid owned by the opposing player, upon which all shots are fired.
+ * 
+ * @author Group c421aa06
+ * 
+ */
+public class FiringControllerImpl implements FiringController {
+
+    /**
+     * The grid being shot at.
+     */
+    private BattleGrid target;
+
+    /**
+     * Sets up the controller with the grid to fire upon.
+     * 
+     * @param target
+     *            the grid owned by the opposing player.
+     */
+    public FiringControllerImpl(BattleGrid target) {
+        this.target = target;
+    }
+
+    @Override
+    public boolean fireShot(int x, int y) {
+        return this.target.shoot(x, y);
+    }
+
+    @Override
+    public boolean isShootable(int x, int y) {
+        return !this.target.isViewable(x, y);
+    }
+
+}
