485754ceaed2d0e6e854337eaf3e08444dc39e8e
unknown <Joe@.(none)>
2011-06-01 17:52:03 -0400

    Cleaned up documentation and methods.

--- src/controllers/WaitingController.java
+++ src/controllers/WaitingController.java
@@ -4,6 +4,12 @@ package controllers;
  * A controller for Player view changing for use in battleship. The user of this
  * interface has control over what causes the view to change.
  * 
+ * @initially <br>
+ *            constructor(BattleshipFrame battleship, Player currentPlayer,
+ *            BattleGrid currentGrid, Player inactivePlayer, BattleGrid
+ *            inactiveGrid): {@code ensures} the WaitingController is associated
+ *            with battleship, both the current and inactive Player, and the
+ *            current and inactive grid.
  * @author Group c421aa06
  */
 public interface WaitingController {
@@ -14,13 +20,13 @@ public interface WaitingController {
 	 * @return the new controller for placing the next player's pieces.
 	 */
 	PlacementController switchPlacementPlayer();
-	
-	/**
-     * Switches the game view between the two Players during firing.
-     * 
-     * @return the new controller with the correct player attacking and defending.
-     */
-    FiringController switchFiringPlayer();
 
+	/**
+	 * Switches the game view between the two Players during firing.
+	 * 
+	 * @return the new controller with the correct player attacking and
+	 *         defending.
+	 */
+	FiringController switchFiringPlayer();
 
 }
