2a05304df07f07ff396d1929218159e0ab2d7c07
Alex Burkhart <saterus@gmail.com>
2011-06-01 16:06:59 -0400

    Basic OrientationSelector component.

---
+++ src/views/OrientationSelector.java
@@ -0,0 +1,31 @@
+package views;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+
+import controllers.PlacementController;
+
+public class OrientationSelector extends JButton {
+
+    private PlacementController controller;
+    
+    /** Follows same convention as the other components, true = x-axis, false = y-axis.*/
+    private boolean orientation;
+
+    public OrientationSelector(PlacementController con) {
+        this.controller = con;
+        this.orientation = true;
+        
+        this.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                
+                OrientationSelector.this.orientation = 
+                    !OrientationSelector.this.orientation;
+                OrientationSelector.this.controller.rotate();
+            }
+        });
+    }
+
+}
