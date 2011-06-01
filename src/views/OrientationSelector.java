c383ad8ed4113758a426c8377099108be72daec7
Karl Racke <racke.3@buckeyemail.osu.edu>
2011-06-01 18:18:40 -0400

    Added button text to when the orientation switches

--- src/views/OrientationSelector.java
+++ src/views/OrientationSelector.java
@@ -9,23 +9,30 @@ import controllers.PlacementController;
 
 public class OrientationSelector extends JButton {
 
-    private PlacementController controller;
-    
-    /** Follows same convention as the other components, true = x-axis, false = y-axis.*/
-    private boolean orientation;
+	private PlacementController controller;
 
-    public OrientationSelector(PlacementController con) {
-        this.controller = con;
-        this.orientation = true;
-        
-        this.addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-                
-                OrientationSelector.this.orientation = 
-                    !OrientationSelector.this.orientation;
-                OrientationSelector.this.controller.rotateShip();
-            }
-        });
-    }
+	/**
+	 * Follows same convention as the other components, true = x-axis, false =
+	 * y-axis.
+	 */
+	private boolean orientation;
 
+	public OrientationSelector(PlacementController con) {
+		this.controller = con;
+		this.orientation = true;
+
+		this.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				OrientationSelector.this.orientation = 
+					!OrientationSelector.this.orientation;
+				OrientationSelector.this.controller.rotateShip();
+			}
+		});
+
+		if (orientation) {
+			this.setText("Change to vertical");
+		} else {
+			this.setText("Change to horizontal.");
+		}
+	}
 }
