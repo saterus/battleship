40b7f0fb82fb40ea7fbbdde21f643e949617a0ae
Alex Burkhart <saterus@gmail.com>
2011-06-01 18:08:42 -0400

    BattleGridView implementation related to Placement.

--- src/views/BattleGridSquare.java
+++ src/views/BattleGridSquare.java
@@ -1,31 +1,64 @@
 package views;
 
+import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.JPanel;
 
+import models.BattleGrid;
 import controllers.PlacementController;
 
 public class BattleGridSquare extends JPanel {
-    
-    private final int x;
-    private final int y;
-    
+
+    private final int          x;
+    private final int          y;
+
+    private static final Color FOG   = Color.LIGHT_GRAY;
+    private static final Color HIT   = Color.RED;
+    private static final Color WATER = Color.BLUE;
+    private static final Color SHIP  = Color.DARK_GRAY;
+
     public BattleGridSquare(int x, int y) {
         this.x = x;
         this.y = y;
-        
+
     }
-    
-    public void addPlacementClickListener(PlacementController con) {
+
+    public void addPlacementClickListener(final PlacementController con) {
+
         this.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
-                
-                con.setShipPos(x, y);
-                // do more stuff
+
+                boolean placementSuccess = con.setShipPos(x, y);
+
+                if (placementSuccess) {
+                    con.disableSelectedShipType();
+                }
             }
         });
     }
 
+    public void setSquareBackground(BattleGrid grid) {
+        Color currentColor;
+        if (grid.getPlayerState()) { // active player owns this grid.
+            if (grid.isShip(this.x, this.y)) {
+                currentColor = BattleGridSquare.SHIP;
+            } else {
+                currentColor = BattleGridSquare.WATER;
+            }
+            
+        } else if (grid.isViewable(this.x, this.y)) {
+            if (grid.isShip(this.x, this.y)) {
+                currentColor = BattleGridSquare.HIT;
+            } else {
+                currentColor = BattleGridSquare.WATER;
+            }
+            
+        } else {
+            currentColor = BattleGridSquare.FOG;
+        }
+
+        this.setBackground(currentColor);
+    }
 }
--- src/views/BattleGridView.java
+++ src/views/BattleGridView.java
@@ -5,28 +5,31 @@ import java.awt.GridLayout;
 import javax.swing.JPanel;
 
 import models.BattleGrid;
-import models.ShipType;
 import controllers.PlacementController;
 
 public class BattleGridView extends JPanel {
-    
+
+    /** Size of the grid used by the game. */
+    private final int        GRID_SIZE;
+
     /** Grid gap size in pixels. */
     private final static int GRID_GAP = 2;
-    
-    public BattleGridView(PlacementController con) {
-        
-        this.setLayout(new GridLayout(BattleGrid.getGridSize(), 
-                                      BattleGrid.getGridSize(), 
-                                      GRID_GAP, GRID_GAP));
-        
-        for(int i = 0; i < BattleGrid.getGridSize(); i++) {
-            for(int j = 0; j < BattleGrid.getGridSize(); j++) {
+
+    public BattleGridView(PlacementController con, BattleGrid grid) {
+
+        GRID_SIZE = grid.gridSize();
+
+        this.setLayout(new GridLayout(GRID_SIZE, GRID_SIZE, GRID_GAP, GRID_GAP));
+
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
                 BattleGridSquare square = new BattleGridSquare(i, j);
-                square.addPlacementClickListener(con);
+                square.addPlacementClickListener(con, grid);
                 this.add(square);
+                
+                square.setSquareBackground(grid);
             }
         }
-
     }
 
 }
