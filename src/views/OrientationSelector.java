2189472162321d275772d49b92952f2d61653686
Alex Burkhart <saterus@gmail.com>
2011-06-01 23:17:20 -0400

    Converted to JPanel. Added logger. Fixed text swapping.

--- src/views/OrientationSelector.java
+++ src/views/OrientationSelector.java
@@ -2,8 +2,12 @@ package views;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.logging.Logger;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import controllers.PlacementController;
 
@@ -12,46 +16,78 @@ import controllers.PlacementController;
  * 
  * @author Group c421aa06
  */
-public class OrientationSelector extends JButton {
+public class OrientationSelector extends JPanel {
 
-	/**
-	 * Serial Version ID.
-	 */
-	private static final long serialVersionUID = 1L;
+    private static final Logger       logger               = Logger.getLogger(OrientationSelector.class
+                                                                   .getName());
 
-	/**
-	 * The PlacementController which uses {@code this}.
-	 */
-	private PlacementController controller;
+    /**
+     * Serial Version ID.
+     */
+    private static final long         serialVersionUID     = 1L;
 
-	/**
-	 * Defines of the current orientation setting where true corresponds to
-	 * orientation along the x-axis and false corresponds to orientation along
-	 * the y-axis.
-	 */
-	private boolean orientation;
+    /**
+     * The PlacementController which uses {@code this}.
+     */
+    private final PlacementController controller;
 
-	/**
-	 * Constructor which creates an OrientationSelector button.
-	 * 
-	 * @param con
-	 *            the PlacementController which uses {@code this}.
-	 */
-	public OrientationSelector(PlacementController con) {
-		this.controller = con;
-		this.orientation = true;
+    /**
+     * Defines of the current orientation setting where true corresponds to
+     * orientation along the x-axis and false corresponds to orientation along
+     * the y-axis.
+     */
+    private boolean                   orientation;
 
-		this.addMouseListener(new MouseAdapter() {
-			public void mouseClicked(MouseEvent e) {
-				OrientationSelector.this.orientation = 
-					!OrientationSelector.this.orientation;
-				OrientationSelector.this.controller.rotateShip();
-			}
-		});
-		if (orientation) {
-			this.setText("Change to vertical");
-		} else {
-			this.setText("Change to horizontal.");
-		}
-	}
+    private final JLabel              label;
+
+    private final JButton             button;
+
+    private final String              northSouthLabelText  = "North/South Placement";
+    private final String              eastWestLabelText    = "East/West Placement";
+
+    private final String              northSouthButtonText = "Change to North/South";
+    private final String              eastWestButtonText   = "Change to East/West";
+
+    /**
+     * Constructor which creates an OrientationSelector button.
+     * 
+     * @param con
+     *            the PlacementController which uses {@code this}.
+     */
+    public OrientationSelector(PlacementController con) {
+        this.controller = con;
+        this.orientation = true;
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        label = new JLabel(eastWestLabelText);
+        this.add(label);
+
+        button = new JButton(northSouthButtonText);
+        this.add(button);
+
+        button.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+
+                OrientationSelector.this.controller.rotateShip();
+
+                OrientationSelector.this.orientation = !OrientationSelector.this.orientation;
+
+                if (orientation) {
+                    OrientationSelector.this.button
+                            .setText(northSouthButtonText);
+                    OrientationSelector.this.label.setText(eastWestLabelText);
+                } else {
+                    OrientationSelector.this.button.setText(eastWestButtonText);
+                    OrientationSelector.this.label.setText(northSouthLabelText);
+                }
+
+                logger.finest("orientation set to : "
+                        + OrientationSelector.this.orientation);
+            }
+        });
+
+        logger.finer("OrientationSelector created.");
+
+    }
 }
