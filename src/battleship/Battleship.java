0efc7c5a4d189ab1114c1737bca1e4fb35a9244b
unknown <Joe@.(none)>
2011-06-02 09:21:56 -0400

    JAVADOC!!!

--- src/battleship/Battleship.java
+++ src/battleship/Battleship.java
@@ -2,17 +2,32 @@ package battleship;
 
 import views.BattleshipFrame;
 
-public class Battleship {
+/**
+ * This class generates a new game of Battleship to be played by two players.
+ * 
+ * @author Group c421aa06
+ */
+public final class Battleship {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
+	/**
+	 * Default constructor. Made private to avoid being used.
+	 */
+	private Battleship() {
 
-        BattleshipFrame game = new BattleshipFrame();
-        game.start();
-        game.setVisible(true);
+	}
 
-    }
+	/**
+	 * The main function which runs the Battleship game.
+	 * 
+	 * @param args
+	 *            the argument array. Battleship requires no arguments.
+	 */
+	public static void main(String[] args) {
+
+		BattleshipFrame game = new BattleshipFrame();
+		game.start();
+		game.setVisible(true);
+
+	}
 
 }
