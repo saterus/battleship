9b56a46bc7491eeb3f743904d76c865479cd5fcd
Alex Burkhart <saterus@gmail.com>
2011-06-02 02:53:06 -0400

    Figured out how to center things.

--- src/views/WaitingView.java
+++ src/views/WaitingView.java
@@ -1,5 +1,9 @@
 package views;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -7,13 +11,20 @@ import controllers.WaitingController;
 
 public final class WaitingView extends JPanel {
 
-    private WaitingController   controller;
+    private final WaitingController   controller;
+    
+    private final JLabel label;
 
-    private final static String messageSuffix = "click when Ready";
+    private final static String messageSuffix = " click when Ready";
 
     public WaitingView(WaitingController con) {
         this.controller = con;
+        
+        this.setLayout(new GridBagLayout());
 
-        this.add(new JLabel(this.controller.getActivePlayer() + WaitingView.messageSuffix));
+        label = new JLabel(this.controller.getActivePlayer().getPlayerName() + WaitingView.messageSuffix);
+        label.setPreferredSize(new Dimension(200, 100));
+        
+        this.add(label, new GridBagConstraints());
     }
 }
