8517fbd66793fa8eb9f114d525224e74b5975820
Alex Burkhart <saterus@gmail.com>
2011-06-02 03:13:17 -0400

    Cycles through Placement now.

--- src/views/WaitingView.java
+++ src/views/WaitingView.java
@@ -3,6 +3,8 @@ package views;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,20 +13,27 @@ import controllers.WaitingController;
 
 public final class WaitingView extends JPanel {
 
-    private final WaitingController   controller;
-    
-    private final JLabel label;
+    private final WaitingController controller;
 
-    private final static String messageSuffix = " click when Ready";
+    private final JLabel            label;
+
+    private final static String     messageSuffix = " click when Ready";
 
     public WaitingView(WaitingController con) {
         this.controller = con;
-        
+
         this.setLayout(new GridBagLayout());
 
-        label = new JLabel(this.controller.getActivePlayer().getPlayerName() + WaitingView.messageSuffix);
+        label = new JLabel(this.controller.getActivePlayer().getPlayerName()
+                + WaitingView.messageSuffix);
         label.setPreferredSize(new Dimension(200, 100));
-        
+
         this.add(label, new GridBagConstraints());
+        
+        this.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                controller.nextScreen();
+            }            
+        });
     }
 }
