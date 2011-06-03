90334bf42c3b956e0af50f20c8118008e53e0076
unknown <Joe@.(none)>
2011-06-03 02:48:30 -0400

    Cleaned up formatting and javadoc

--- src/controllers/FiringControllerImpl.java
+++ src/controllers/FiringControllerImpl.java
@@ -15,77 +15,86 @@ import models.ShipType;
  */
 public final class FiringControllerImpl implements FiringController {
 
-    private static final Logger     LOGGER              = Logger.getLogger(FiringControllerImpl.class
-                                                                .getName());
+	/**
+	 * A logger for use with the FiringControllerImpl class.
+	 */
+	private static final Logger LOGGER = Logger
+			.getLogger(FiringControllerImpl.class.getName());
 
-    /**
-     * The grid being shot at.
-     */
-    private final BattleGrid        target;
+	/**
+	 * The grid being shot at.
+	 */
+	private final BattleGrid target;
 
-    /**
-     * The WaitingController to be used with firing.
-     */
-    private final WaitingController waiting;
+	/**
+	 * The WaitingController to be used with firing.
+	 */
+	private final WaitingController waiting;
 
-    private static final String     sunkMessageTemplate = " has sunk your ";
+	/**
+	 * The message template to be displayed upon a Ship sinking.
+	 */
+	private final String sunkMessageTemplate = " has sunk your ";
 
-    private String                  sunkMessage;
+	/**
+	 * The message to be displayed upon a Ship sinking.
+	 */
+	private String sunkMessage;
 
-    /**
-     * Sets up the controller with the grid to fire upon.
-     * 
-     * @param waiting
-     *            the WaitingController to be used with firing.
-     * @param target
-     *            the grid owned by the opposing player.
-     */
-    public FiringControllerImpl(WaitingController waiting, BattleGrid target) {
-        this.waiting = waiting;
-        this.target = target;
-        this.sunkMessage = null;
-    }
+	/**
+	 * Sets up the controller with the grid to fire upon.
+	 * 
+	 * @param waiting
+	 *            the WaitingController to be used with firing.
+	 * @param target
+	 *            the grid owned by the opposing player.
+	 */
+	public FiringControllerImpl(WaitingController waiting, BattleGrid target) {
+		this.waiting = waiting;
+		this.target = target;
+		this.sunkMessage = null;
+	}
 
-    @Override
-    public HitStatus fireShot(int x, int y) {
+	@Override
+	public HitStatus fireShot(int x, int y) {
 
-        HitStatus outcome = this.target.shoot(x, y);
+		HitStatus outcome = this.target.shoot(x, y);
 
-        if (HitStatus.SUNK == outcome) {
-            ShipType t = this.target.shipTypeAt(x, y);
-            this.sunkMessage = waiting.getActivePlayer().getPlayerName()
-                    + this.sunkMessageTemplate + t.toString();
+		if (HitStatus.SUNK == outcome) {
+			ShipType t = this.target.shipTypeAt(x, y);
+			this.sunkMessage = waiting.getActivePlayer().getPlayerName()
+					+ this.sunkMessageTemplate + t.toString();
 
-            LOGGER.info(t.toString() + " has been sunk!");
-        }
+			LOGGER.info(t.toString() + " has been sunk!");
+		}
 
-        if (!this.target.shipsRemaining()) {
-            this.waiting.setHasWon(true);
-            this.sunkMessage += waiting.getActivePlayer().getPlayerName()
-                    + " has sunk all your ships and won the game!";
+		if (!this.target.shipsRemaining()) {
+			this.waiting.setHasWon(true);
+			this.sunkMessage += waiting.getActivePlayer().getPlayerName()
+					+ " has sunk all your ships and won the game!";
 
-            LOGGER.info(waiting.getActivePlayer().getPlayerName() + " has won!");
-        }
+			LOGGER.info(waiting.getActivePlayer().getPlayerName() + " has won!");
+		}
 
-        return outcome;
-    }
+		return outcome;
+	}
 
-    @Override
-    public boolean isShootable(int x, int y) {
-        return !this.target.isViewable(x, y);
-    }
+	@Override
+	public boolean isShootable(int x, int y) {
+		return !this.target.isViewable(x, y);
+	}
 
-    @Override
-    public BattleGrid getTarget() {
-        return this.target;
-    }
+	@Override
+	public BattleGrid getTarget() {
+		return this.target;
+	}
 
-    @Override
-    public void endTurn() {
-        String tempMsg = this.sunkMessage;
-        this.sunkMessage = null;
-        
-        this.waiting.nextScreen(tempMsg);
-    }
+	@Override
+	public void endTurn() {
+		String tempMsg = this.sunkMessage;
+		this.sunkMessage = null;
+
+		this.waiting.nextScreen(tempMsg);
+	}
 
 }
